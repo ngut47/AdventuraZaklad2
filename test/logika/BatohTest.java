4ceeca5072966e32ff997f2d4dbb2ce0f4b953ef
Tran <ngut47@vse.cz>
2020-10-12 14:44:05 +0200

    prvotni

---
+++ test/logika/BatohTest.java
@@ -0,0 +1,28 @@
+package logika;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BatohTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void vlozVec() {
+    }
+
+    @Test
+    public void nazvyVeci() {
+    }
+
+    @Test
+    public void vyberVec() {
+    }
+}
